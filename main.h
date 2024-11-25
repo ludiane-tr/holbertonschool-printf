88f4a91bf7eed0eb58d5a12b1911b8443d649c09
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-11-25 11:23:43 +0100

    create the main.c and main.h

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <stddef.h>
+#include <stdarg.h>
+
+/* Prototype */
+
+int _printf(const char *format, ...);
+
+#endif
