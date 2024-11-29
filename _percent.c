ad4eb5e943f0ff78e111971ec2a00978dc678e82
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-11-29 11:31:20 +0100

    update the files

---
+++ _percent.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _percent - print %.
+ * @args: list of arguments.
+ * Return: number of chars.
+ */
+int _percent(va_list args)
+{
+	(void)args;
+
+	_putchar('%');
+
+	return (1);
+}
